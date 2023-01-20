1fb1245d65b7b68876935049ce9f2a1e9b2f55ef
Bighdawg15 <122480357+Bighdawg15@users.noreply.github.com>
2023-01-19 19:22:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require(`discord.js`);
+
+const prefix = '>';
+
+const client = new Client ({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+    console.log("Bot is online!");
+
+    client.user.setActivity('Testing the bot in the tutorial be like....', { type: "EXISTING" });
+
+})
+
+
+client.login("MTA2NTc1NzQ1MjkyNDA5NjU4Mg.GOJiRW.0xYcfAvZrUS3ErtEhIYhSQqcUI0AApxcHnFOBc");
